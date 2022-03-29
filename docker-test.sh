690d702b37b5bea0aa3a518351409ba093e90786
Josh Peak <josh.peak@intellify.com.au>
2022-03-30 01:06:12 +1100

    Add basic docker testing matrix tooling

---
+++ docker-test.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+python --version
+
+# Mount local source code to /opt/var/invoke
+# Make a clone into ~/invoke to avoid volume mapping issues
+cd ~
+git clone /opt/var/invoke ./invoke/
+cd invoke/
+
+echo "Upgrading pip..."
+python -m pip install -U pip -qq
+echo "Installing deps via -r docker-test-requirements.txt ..."
+python -m pip install -r /opt/var/invoke/docker-test-requirements.txt -qq
+
+pytest --color=yes --capture=no
+# inv test
